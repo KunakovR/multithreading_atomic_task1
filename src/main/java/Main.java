c71588e68d79d135d469d655aa16f6270af08ebc
Ruslan <>
2022-11-25 15:14:04 +0700

    2nd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,12 @@
-import java.util.Random;
-
-
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        Count count = new Count();
+        MyThread myThread = new MyThread();
 
-        Thread thread1 = new Thread(null, () -> count.countNickname(3), "Красивых слов с длиной 3: ");
-        Thread thread2 = new Thread(null, () -> count.countNickname(4), "Красивых слов с длиной 4: ");
-        Thread thread3 = new Thread(null, () -> count.countNickname(5), "Красивых слов с длиной 5: ");
+        Thread thread1 = new Thread(null, () -> myThread.countNickname(3), "Красивых слов с длиной 3: ");
+        Thread thread2 = new Thread(null, () -> myThread.countNickname(4), "Красивых слов с длиной 4: ");
+        Thread thread3 = new Thread(null, () -> myThread.countNickname(5), "Красивых слов с длиной 5: ");
 
         thread1.start();
         thread1.join();
@@ -17,14 +14,5 @@ public class Main {
         thread2.join();
         thread3.start();
         thread3.join();
-
-
-
-
-
-
-
-
     }
-
 }
